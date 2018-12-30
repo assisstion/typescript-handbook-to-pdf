875ced3141aa961668ad85fc34c36ac28e236b30
Markus Feng <markusfeng@gmail.com>
2018-12-30 14:42:29 +0800

    Modified output pdf so now it is in order (as presented on the Typescript website)

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,46 @@ import createFiles, { Files } from './files';
 import makePdf from './pdf';
 
 (async () => {
+
+    const names =
+    ['Basic Types',
+    'Variable Declarations',
+    'Interfaces',
+    'Classes',
+    'Functions',
+    'Generics',
+    'Enums',
+    'Type Inference',
+    'Type Compatibility',
+    'Advanced Types',
+    'Symbols',
+    'Iterators and Generators',
+    'Modules',
+    'Namespaces',
+    'Namespaces and Modules',
+    'Module Resolution',
+    'Declaration Merging',
+    'JSX',
+    'Decorators',
+    'Mixins',
+    'Triple-Slash Directives',
+    'Type Checking JavaScript Files']
+
     const cwd = process.cwd();
     const githubClient: GithubClient = createGithubClient(execa);
     const files: Files = createFiles(fs);
     const { log } = console;
 
     log('Cloning handbook repo...');
-    const clonedRepoDir: string = 
+    const clonedRepoDir: string =
         await githubClient.clone('Microsoft/TypeScript-Handbook', path.join(cwd, 'temp'));
     log('Done');
+    const pathNames = names.map(x => path.join(clonedRepoDir, 'pages', x + '.md'));
     const markdownFiles: string[] = files.findMarkdown(path.join(clonedRepoDir, 'pages'));
+    const orderedNames = pathNames.filter(x => markdownFiles.indexOf(x) >= 0);
+    const remainingNames = markdownFiles.filter(x => pathNames.indexOf(x) < 0);
+    const orderedMarkdownFiles = orderedNames.concat(remainingNames);
     log('Creating pdf...');
-    await makePdf(mpdf().concat, markdownFiles, path.join(cwd, 'handbook.pdf'));
+    await makePdf(mpdf().concat, orderedMarkdownFiles, path.join(cwd, 'handbook.pdf'));
     log('Done');
 })();
